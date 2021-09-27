ff52dc0f427057a7ed8501711cd51b400ccc2f5a
sth2468 <60360920+sth2468@users.noreply.github.com>
2021-09-27 13:12:14 -0700

    fix controller paths

--- routes/proveRoutes/admin.js
+++ routes/proveRoutes/admin.js
@@ -1,8 +1,6 @@
-const path = require('path');
-
 const express = require('express');
 
-const productsController = require('../controllers/w03/products');
+const productsController = require('../../controllers/w03/products');
 
 const router = express.Router();
 
