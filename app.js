5cda6ca70b1d1a1f6f841dfcc6e42017314d5c38
sth2468 <60360920+sth2468@users.noreply.github.com>
2021-10-08 08:08:48 -0700

    fix user id

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('615fd4f77237b046b2e1082b')
     .then(user => {
       req.user = user;
       next();
